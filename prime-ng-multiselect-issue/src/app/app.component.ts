6ec1029f6f295d2f84229fa9b5a9905838746baa
Selmi Abdelaziz <Abdelaziz.Selmi@admiral.at>
2025-03-17 13:52:49 +0100

    Add multiselect in dialog to showcase alignment issue of the overlay panel when in rtl mode

--- prime-ng-multiselect-issue/src/app/app.component.ts
+++ prime-ng-multiselect-issue/src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {Button} from "primeng/button";
+import {Dialog} from "primeng/dialog";
+import {MultiSelect} from "primeng/multiselect";
+import {FormsModule} from "@angular/forms";
+import {ToggleSwitch} from "primeng/toggleswitch";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [Button, Dialog, MultiSelect, FormsModule, ToggleSwitch],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'prime-ng-multiselect-issue';
+  visible: boolean = false;
+  rtlEnabled: boolean = false;
+  cities: any[] = [];
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.cities.push({
+        code: i,
+        name: `City ${i}`
+      });
+    }
+  }
+
+  toggleRTL() {
+    const htmlElement = document.documentElement;
+
+    if (this.rtlEnabled) {
+      htmlElement.setAttribute("dir", "rtl");
+    } else {
+      htmlElement.removeAttribute("dir");
+    }
+  }
 }
